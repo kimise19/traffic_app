5b3f4a2b33046edf90a4414bffcda1c8ed7c1400
tom mise <kimise@espe.edu.ec>
2024-02-23 01:29:10 -0500

    PDF DOWNLOADER

--- src/componets/LawPage.jsx
+++ src/componets/LawPage.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View, Text, Image, TouchableOpacity, StyleSheet } from 'react-native';
+import { View, Text, Image, TouchableOpacity, StyleSheet, Linking } from 'react-native';
 import Icon from 'react-native-vector-icons/Ionicons';
 
 const Header = ({ navigation }) => (
@@ -10,19 +10,36 @@ const Header = ({ navigation }) => (
   </View>
 );
 
-const LawPage = ({ navigation }) => {
+const LawPage = () => {
+  const pdfUrl = 'https://www.obraspublicas.gob.ec/wp-content/uploads/downloads/2021/08/LOTAIP_6_Ley-Organica-de-Transporte-Terrestre-Transito-y-Seguridad-Vial-2021.pdf';
+
+  // Función para manejar la apertura del enlace con aplicaciones externas
+  const handleOpenLink = async () => {
+    try {
+      const supported = await Linking.canOpenURL(pdfUrl);
+
+      if (supported) {
+        await Linking.openURL(pdfUrl);
+      } else {
+        console.error('No se puede abrir el enlace:', pdfUrl);
+      }
+    } catch (error) {
+      console.error('Error al intentar abrir el enlace:', error);
+    }
+  };
+
   return (
     <View style={styles.container}>
-      <Header navigation={navigation} />
+      <Header />
       <View style={styles.contentContainer}>
         <Text style={styles.title}>Leyes de Tránsito</Text>
         <Image
-          source={{
-            uri: 'https://transitoecuador.com/wp-content/uploads/nueva-ley-de-transito-ecuador-2021.jpg', // 
-          }}
+          source={require("./images/leyes_logo.jpg")}
           style={styles.image}
         />
-        <TouchableOpacity style={styles.button} onPress={() => console.log('Descargar')}>
+
+        {/* Botón para abrir el enlace con aplicaciones externas */}
+        <TouchableOpacity style={styles.button} onPress={handleOpenLink}>
           <Text style={styles.buttonText}>Descargar</Text>
         </TouchableOpacity>
       </View>
@@ -31,50 +48,48 @@ const LawPage = ({ navigation }) => {
 };
 
 const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      justifyContent: 'center',
-      alignItems: 'center',
-      backgroundColor: '#2C2C38',
-    },
-    contentContainer: {
-      justifyContent: 'center',
-      alignItems: 'center',
-    },
-    title: {
-      fontSize: 27,
-      fontWeight: 'bold',
-      marginBottom: 10,
-      color: '#63B5E5',
-      padding:  10,
-    },
-    image: {
-      width: 400,
-      height:400,
-      resizeMode: 'cover',
-      marginBottom: 10,
-    },
-    button: {
-      width: 250,
-      height: 60,
-      backgroundColor: '#63B5E5',
-      justifyContent: 'center',
-      alignItems: 'center',
-      borderRadius: 5,
-      marginTop: 20,
-    },
-    buttonText: {
-      color: '#fff',
-      fontSize: 16,
-      textAlign: 'center',
-    },
-    headerContainer: {
-      position: 'absolute',
-      top: 20,
-      left:20,
-    },
-   
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#2C2C38',
+  },
+  contentContainer: {
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  title: {
+    fontSize: 27,
+    fontWeight: 'bold',
+    marginBottom: 10,
+    color: '#63B5E5',
+    padding: 10,
+  },
+  image: {
+    width: 400,
+    height: 400,
+    resizeMode: 'cover',
+    marginBottom: 10,
+  },
+  button: {
+    width: 250,
+    height: 60,
+    backgroundColor: '#63B5E5',
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 5,
+    marginTop: 20,
+  },
+  buttonText: {
+    color: '#fff',
+    fontSize: 16,
+    textAlign: 'center',
+  },
+  headerContainer: {
+    position: 'absolute',
+    top: 20,
+    left: 20,
+  },
+});
 
-  });
-  
-  export default LawPage;
+export default LawPage;
